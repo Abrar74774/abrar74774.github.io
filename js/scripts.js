65f4efa49cfe8c5de8e0478a4597e9f26661c312
Abrar74774 <abrarshahriarhossain@gmail.com>
2020-01-25 00:14:21 +0300

    bit of redesign

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-var obj = {
-    hello:'hehe',
-    lello:'lele'
-}
+var fi1 = document.getElementById("proj");
+var fi2 = document.getElementById("portfolio");
+var fi3 = document.getElementById("about");
+var fi4 = document.getElementById("contact");
+
+
+window.addEventListener('scroll', function() {
+    console.log(pageYOffset);
+    if (pageYOffset >= 170) {fi1.style.opacity = 1};
+    if (pageYOffset >= 570) {fi2.style.opacity = 1};
+    if (pageYOffset >= 1400) {fi3.style.opacity = 1};
+    if (pageYOffset >= 1870) {fi4.style.opacity = 1};
+})
