1ca72fbca562a53328ab2625e4f1fb3fc17ea5af
Abrar74774 <abrarshahriarhossain@gmail.com>
2020-02-01 14:54:15 +0300

    changed file directiory

---
+++ dist/js/scripts.js
@@ -0,0 +1,52 @@
+var mainFadeIn = document.querySelector('.hello');
+var fi1 = document.getElementById("proj");
+var fi2 = document.getElementById("portfolio");
+var fi3 = document.getElementById("about");
+var fi4 = document.getElementById("contact");
+
+// Intersection Observer========
+var options = {
+    rootMargin: '-45%',
+}
+
+var observer = new IntersectionObserver(function(entries) {
+    console.log(entries);
+    if (entries[0].isIntersecting) {
+        entries[0].target.style.opacity = 1;
+    }
+}, options)
+
+observer.observe(fi1);
+observer.observe(fi2);
+observer.observe(fi3);
+observer.observe(fi4);
+//===============================
+
+// Start fade in
+window.addEventListener('load', function() {
+    mainFadeIn.style.opacity = 1;
+})
+
+
+
+
+// //Responsive fade on scroll
+// var width = window.innerWidth;
+// var addedScroll = 600;    
+// if (width >= 872) {addedScroll = 200;};
+// if (width >= 1308) {addedScroll = 0}
+// // for (skills) expanding
+// var addedScroll2 = 0;
+// if (width >= 787) {addedScroll2 = 0};
+
+
+ 
+
+// window.addEventListener('scroll', function() {
+//     console.log(pageYOffset);
+//     if (pageYOffset >= 170) {fi1.style.opacity = 1};
+//     if (pageYOffset >= 570) {fi2.style.opacity = 1};
+//     if (pageYOffset >= 1400 + addedScroll) {fi3.style.opacity = 1};
+//     if (pageYOffset >= 1870 + addedScroll) {fi4.style.opacity = 1};
+// })
+
