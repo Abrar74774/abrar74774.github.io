c7298958daba746b673e9d0f4120614fdffbc1b3
Abrar74774 <abrarshahriarhossain@gmail.com>
2020-05-29 23:09:23 +0300

    redesigned

--- js/portfolio.js
+++ js/portfolio.js
@@ -16,30 +16,29 @@ var div6 = document.getElementById("f");
 
 //===================================Function=========================================================
 
-function popupEffect(data)  {
+function popupEffect(data) {
   console.log(data);
-  popup.innerHTML = 
+  popup.innerHTML =
 
-      `
+    `
       <div id="close">&times;</div>
       <div class="content">
         <div class="view" style="background-image:url('${data.img}');"></div>
         <div class="lang" data-simplebar >
           <h2>${data.title}</h2>
-          <h3>Technologies used:</h3>
           <ul>
-            ${data.lang.map(lang =>'<li>' + lang + '</li>').join('')}
+            ${data.lang.map(lang => '<li>' + lang + '</li>').join('')}
           </ul>
           <br>
-          <a href="${data.site}">Visit Site</a><a href="${data.source}">View Source on GitHub</a>
+          <a href="${data.site}" target="_blank">Visit Site</a><a href="${data.source}" target="_blank">View Source on GitHub</a>
           <p>${data.description}</p>
         </div>
       </div>
       ` ;
   popup.style.zIndex = '99999999';
-  popup.style.opacity = '1'; 
+  popup.style.opacity = '1';
   overlay.style.zIndex = '3';
-  overlay.style.opacity='0.7';
+  overlay.style.opacity = '0.7';
   //popupclose====
   var close = document.getElementById("close");
   close.addEventListener("click", closepopup);
@@ -51,35 +50,35 @@ function popupEffect(data)  {
 
 //Portfolio popup
 
-div1.addEventListener("click", function() {
+div1.addEventListener("click", function () {
   popupEffect(data1);
 });
-div2.addEventListener("click", function() {
+div2.addEventListener("click", function () {
   popupEffect(data2);
 });
-div3.addEventListener("click", function() {
+div3.addEventListener("click", function () {
   popupEffect(data3);
 });
-div4.addEventListener("click", function() {
+div4.addEventListener("click", function () {
   popupEffect(data4);
 });
-div5.addEventListener("click", function() {
+div5.addEventListener("click", function () {
   popupEffect(data5);
 });
-div6.addEventListener("click", function() {
+div6.addEventListener("click", function () {
   popupEffect(data6);
 });
 
 //Portfolio popup close
 
 function closepopup() {
-  
+
   console.log("closing attempted")
   overlay.style.opacity = '0';
-  setTimeout(function(){
+  setTimeout(function () {
     popup.style.zIndex = '-9';
     overlay.style.zIndex = '-999';
-  }, getComputedStyle(popup).transitionDuration.replace('s','') * 1000)
+  }, getComputedStyle(popup).transitionDuration.replace('s', '') * 1000)
   popup.style.opacity = '0';
 }
 overlay.addEventListener("click", closepopup);
@@ -89,55 +88,55 @@ overlay.addEventListener("click", closepopup);
 // =================================Individual Project Data=================================================
 
 var data1 = {
-  title:"React Weather App",
-  lang:["React","Rest API", "CSS", "HTML"],
-  site:"https://abrar74774.github.io/ReactWeatherApp",
-  source:"https://github.com/Abrar74774/ReactWeatherApp",
-  description:"This is an app, where user enters the location or location coordinates, and the app displays current temperature of the location with highest and lowest temperature of the day, along with those of the next five days.<br><br>The app is built with React. It uses fetch API for fetching data from arcgis autocomplete REST API, and searches weather data from DarkSky API, and displays it. It uses React Router for showing two pages of the app.",
-  img:"./img/reactweatherapp2.png"
+  title: "React Weather App",
+  lang: ["React", "Rest API", "CSS", "HTML"],
+  site: "https://abrar74774.github.io/ReactWeatherApp",
+  source: "https://github.com/Abrar74774/ReactWeatherApp",
+  description: "This is an app, where user enters the location or location coordinates, and the app displays current temperature of the location with highest and lowest temperature of the day, along with those of the next five days.<br><br>The app is built with React. It uses fetch API for fetching data from arcgis autocomplete REST API, and searches weather data from DarkSky API, and displays it. It uses React Router for showing two pages of the app.",
+  img: "./img/reactweatherapp2.png"
 }
 
 var data2 = {
-  title:"Dictionary App",
-  lang:["Javascript", "REST API (OwlBot)", "CSS", "HTML"],
-  site:"https://abrar74774.github.io/Dictionary/",
-  source:"https://github.com/Abrar74774/Dictionary",
-  description:"This is a dictionary app. It takes a word as user input, and displays the meaning and description of the word along with the word type.<br><br>The app uses Javascript ES6's fetch API to fetch data from OwlBot API.",
-  img:'./img/dict.jpg'
+  title: "Dictionary App",
+  lang: ["Javascript", "REST API (OwlBot)", "CSS", "HTML"],
+  site: "https://abrar74774.github.io/Dictionary/",
+  source: "https://github.com/Abrar74774/Dictionary",
+  description: "This is a dictionary app. It takes a word as user input, and displays the meaning and description of the word along with the word type.<br><br>The app uses Javascript ES6's fetch API to fetch data from OwlBot API.",
+  img: './img/dict.jpg'
 }
 
 var data3 = {
-  title:"Doodle Clock",
-  lang:["Javascript", "CSS", "HTML"],
-  site:"https://abrar74774.github.io/myClock",
-  source:"https://github.com/Abrar74774/myClock",
-  description:"This is a Javascript clock, which is designed with the most famous design tool in the world, MS Paint.",
-  img:'./img/clock.jpg'
+  title: "Doodle Clock",
+  lang: ["Javascript", "CSS", "HTML"],
+  site: "https://abrar74774.github.io/myClock",
+  source: "https://github.com/Abrar74774/myClock",
+  description: "This is a Javascript clock, which is designed with the most famous design tool in the world, MS Paint.",
+  img: './img/clock.jpg'
 }
 
 var data4 = {
-  title:"Construction Website",
-  lang:["CSS", "HTML", "Javascript"],
-  site:"https://abrar74774.github.io/Construction-Website",
-  source:"https://github.com/Abrar74774/Construction-Website",
-  description:"This is a construction company website.<br> The page is designed with CSS, it is responsive, and has some javascript effects.",
-  img:'./img/const.jpg'
+  title: "Construction Website",
+  lang: ["CSS", "HTML", "Javascript"],
+  site: "https://abrar74774.github.io/Construction-Website",
+  source: "https://github.com/Abrar74774/Construction-Website",
+  description: "This is a construction company website.<br> The page is designed with CSS, it is responsive, and has some javascript effects.",
+  img: './img/const.jpg'
 }
 
 var data5 = {
-  title:"Word Counter App",
-  lang:["Javascript","CSS", "HTML"],
-  site:"https://abrar74774.github.io/Word-Counter",
-  source:"https://github.com/Abrar74774/Word-Counter",
-  description:"This is a Word Counter App built with Javascript.",
-  img:'./img/wc.jpg'
+  title: "Word Counter App",
+  lang: ["Javascript", "CSS", "HTML"],
+  site: "https://abrar74774.github.io/Word-Counter",
+  source: "https://github.com/Abrar74774/Word-Counter",
+  description: "This is a Word Counter App built with Javascript.",
+  img: './img/wc.jpg'
 }
 
 var data6 = {
-  title:"Restaurant Website",
-  lang:["CSS", "HTML","Javascript"],
-  site:"https://abrar74774.github.io/Restaurant",
-  source:"https://github.com/Abrar74774/Restaurant",
-  description:"This is a Restaurant Website, which is responsive, designed with CSS.",
-  img:'./img/rest.jpg'
+  title: "Restaurant Website",
+  lang: ["CSS", "HTML", "Javascript"],
+  site: "https://abrar74774.github.io/Restaurant",
+  source: "https://github.com/Abrar74774/Restaurant",
+  description: "This is a Restaurant Website, which is responsive, designed with CSS.",
+  img: './img/rest.jpg'
 }
--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ var options = {
 var observer = new IntersectionObserver(function (entries) {
     console.log(entries);
     if (entries.filter(entry => entry.isIntersecting)) {
-        console.log(entries.filter(entry => entry.isIntersecting).map(entry => entry.target.style.opacity = 1))
+        entries.filter(entry => entry.isIntersecting).map(entry => entry.target.style.opacity = 1);
     }
 }, options)
 
@@ -26,31 +26,5 @@ observer.observe(fi4);
 //===============================
 
 // Start fade in
-window.addEventListener('load', function () {
-    mainFadeIn.style.opacity = 1;
 
-})
-
-
-
-
-// //Responsive fade on scroll
-// var width = window.innerWidth;
-// var addedScroll = 600;    
-// if (width >= 872) {addedScroll = 200;};
-// if (width >= 1308) {addedScroll = 0}
-// // for (skills) expanding
-// var addedScroll2 = 0;
-// if (width >= 787) {addedScroll2 = 0};
-
-
-
-
-// window.addEventListener('scroll', function() {
-//     console.log(pageYOffset);
-//     if (pageYOffset >= 170) {fi1.style.opacity = 1};
-//     if (pageYOffset >= 570) {fi2.style.opacity = 1};
-//     if (pageYOffset >= 1400 + addedScroll) {fi3.style.opacity = 1};
-//     if (pageYOffset >= 1870 + addedScroll) {fi4.style.opacity = 1};
-// })
-
+mainFadeIn.style.opacity = 1;
