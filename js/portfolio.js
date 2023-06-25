d9dff6e8160a5e2d6555983284203637f7015253
Abrar74774 <abrarshahriarhossain@gmail.com>
2023-06-25 21:13:15 +0300

    Add animation for portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,13 +15,6 @@ let projDivs = [
   document.getElementById("f"),
 ]
 
-// var div1 = document.getElementById("a");
-// var div2 = document.getElementById("b");
-// var div3 = document.getElementById("c");
-// var div4 = document.getElementById("d");
-// var div5 = document.getElementById("e");
-// var div6 = document.getElementById("f");
-
 // =================================Individual Project Data=================================================
 
 let data = [
@@ -124,85 +117,5 @@ projDivs.forEach((div, index) => {
   div.querySelector(".learn-more").innerText = data[index].lang.join(", ")
 })
 
-// div1.addEventListener("click", function () {
-//   popupEffect(data1);
-// });
-// div2.addEventListener("click", function () {
-//   popupEffect(data2);
-// });
-// div3.addEventListener("click", function () {
-//   popupEffect(data3);
-// });
-// div4.addEventListener("click", function () {
-//   popupEffect(data4);
-// });
-// div5.addEventListener("click", function () {
-//   popupEffect(data5);
-// });
-// div6.addEventListener("click", function () {
-//   popupEffect(data6);
-// });
-
-//Portfolio popup close
-
 overlay.addEventListener("click", closepopup);
 
-// ================================= Set Titles ============================================================
-
-
-
-
-
-// var data1 = {
-//   title: "Angular Data Visualizer",
-//   lang: ["Angular", "Rest API", "CSS", "HTML"],
-//   site: "https://abrar74774.github.io/AngularDataVisualizer/",
-//   source: "https://github.com/Abrar74774/AngularDataVisualizer",
-//   description: "Visualize input data as bar chart, line chart or pie chart. Uses Chart.js",
-//   img: "./img/datvis.jpg"
-// }
-
-// var data2 = {
-//   title: "Dictionary App",
-//   lang: ["Javascript", "REST API (OwlBot)", "CSS", "HTML"],
-//   site: "https://abrar74774.github.io/Dictionary/",
-//   source: "https://github.com/Abrar74774/Dictionary",
-//   description: "This is a dictionary app. It takes a word as user input, and displays the meaning and description of the word along with the word type.<br><br>The app uses Javascript ES6's fetch API to fetch data from OwlBot API.",
-//   img: './img/dict.jpg'
-// }
-
-// var data3 = {
-//   title: "Doodle Clock",
-//   lang: ["Javascript", "CSS", "HTML"],
-//   site: "https://abrar74774.github.io/myClock",
-//   source: "https://github.com/Abrar74774/myClock",
-//   description: "This is a Javascript clock, which is designed with the most famous design tool in the world, MS Paint.",
-//   img: './img/clock.jpg'
-// }
-
-// var data4 = {
-//   title: "Construction Website",
-//   lang: ["CSS", "HTML", "Javascript"],
-//   site: "https://abrar74774.github.io/Construction-Website",
-//   source: "https://github.com/Abrar74774/Construction-Website",
-//   description: "This is a construction company website.<br> The page is designed with CSS, it is responsive, and has some javascript effects.",
-//   img: './img/const.jpg'
-// }
-
-// var data5 = {
-//   title: "Word Counter App",
-//   lang: ["Javascript", "CSS", "HTML"],
-//   site: "https://abrar74774.github.io/Word-Counter",
-//   source: "https://github.com/Abrar74774/Word-Counter",
-//   description: "This is a Word Counter App built with Javascript.",
-//   img: './img/wc.jpg'
-// }
-
-// var data6 = {
-//   title: "Restaurant Website",
-//   lang: ["CSS", "HTML", "Javascript"],
-//   site: "https://abrar74774.github.io/Restaurant",
-//   source: "https://github.com/Abrar74774/Restaurant",
-//   description: "This is a Restaurant Website, which is responsive, designed with CSS.",
-//   img: './img/rest.jpg'
-// }
--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,26 @@ var close = document.getElementById("close");
 
 // Intersection Observer========
 var options = {
-    rootMargin: '-33% 0%'
+    rootMargin: '-45% 0%'
 }
 
 var observer = new IntersectionObserver(function (entries) {
     console.log(entries);
     if (entries.filter(entry => entry.isIntersecting)) {
-        entries.filter(entry => entry.isIntersecting).map(entry => entry.target.style.opacity = 1);
+        entries.filter(entry => entry.isIntersecting).map(entry => {
+            entry.target.style.opacity = 1;
+            if (entry.target.id == "portfolio") {
+                anime({
+                    targets: '.projects',
+                    translateY: [-20, 0],
+                    opacity: 1,
+                    easing: 'easeOutSine',
+                    delay: anime.stagger(150),
+                    duration: 500
+                });
+            }
+            observer.unobserve(entry.target)
+        });
     }
 }, options)
 
